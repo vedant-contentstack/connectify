41a52496688f371ff467e84254aeaa5070455d7d
Vedant Karle <vedant.karle@contentstack.com>
2025-10-21 12:56:05 +0530

    feat: db setup

---
+++ src/lib/db.ts
@@ -0,0 +1,15 @@
+//This will help avoid creating prisma instances every hot reload
+
+import { PrismaClient } from "@/generated/prisma";
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
